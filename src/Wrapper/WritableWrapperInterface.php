e7b9fb6513be6085fcf64482ebfa463526586e84
Serafim <nesk@xakep.ru>
2019-01-01 22:29:21 +0300

    Fix headers

--- src/Wrapper/WritableWrapperInterface.php
+++ src/Wrapper/WritableWrapperInterface.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * This file is part of Reflect\Streaming package.
+ * This file is part of Stream package.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
